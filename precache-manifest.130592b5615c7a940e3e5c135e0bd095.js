155ba15cf29ce2f58f53a077c19aecccf7c404e5
Gabriel Seals <sealsgj@gmail.com>
2020-07-24 17:02:03 -0500

    Updates

---
+++ precache-manifest.130592b5615c7a940e3e5c135e0bd095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b399adad7d5d0b9b6a2d29483156f57e",
+    "url": "/quizbee/index.html"
+  },
+  {
+    "revision": "fb833d7cd08994e380e9",
+    "url": "/quizbee/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "4c7ccea165daa7e886e8",
+    "url": "/quizbee/static/js/2.69ec6805.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizbee/static/js/2.69ec6805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb833d7cd08994e380e9",
+    "url": "/quizbee/static/js/main.79cfbacf.chunk.js"
+  },
+  {
+    "revision": "0449a30c061fe65aa1db",
+    "url": "/quizbee/static/js/runtime-main.fb96dcd2.js"
+  }
+]);
